fd8b5e77db718a3b13c1afb44e0b660dd0bf3ea5
Ryan Arnold <rja87.sd@gmail.com>
2024-01-22 16:52:17 -0600

    Add styling and comments.

--- app.js
+++ app.js
@@ -2,22 +2,30 @@
 
 const form = document.querySelector("#myForm");
 
+// This listens for the form to be submitted
 form.addEventListener("submit", (e) => {
+  // If the number of characters in the username is less than 5, prevent default and alert the user with the message below.
   if (form.username.value < 5) {
     e.preventDefault();
     alert("Username must be at least 5 characters");
   }
 
-  if (form.assignedSlot.value < 18 || form.assignedSlot.value > 120 || form.assignedSlot.value == ""){
+  // If the age value is less than 18 or greater than 120 or blank, prevent default and alert the user with the message below.
+  if (
+    form.assignedSlot.value < 18 ||
+    form.assignedSlot.value > 120 ||
+    form.assignedSlot.value == ""
+  ) {
     e.preventDefault();
-    alert('Enter a valid age.');
+    alert("Enter a valid age.");
   }
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  // This regex depicts the acceptable format for entering an email address.
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  if(!emailRegex.test(form.email.value)){
+  // Test the entered data against the regex and if it is true, make it false and prevent default and alert the user with the message below.
+  if (!emailRegex.test(form.email.value)) {
     e.preventDefault();
-    alert('Enter a valid email address.');
+    alert("Enter a valid email address.");
   }
 });
-
